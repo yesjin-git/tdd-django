667d13a35420973e61a13394794c801e6679acea
yesjin-git <tiuln2010@gmail.com>
2019-04-21 23:42:56 +0900

    init

---
+++ functional_tests.py
@@ -0,0 +1,44 @@
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+  def setUp(self):
+    self.browser = webdriver.Edge()
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    self.browser.get('http://localhost:8000')
+
+    self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
+
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      '작업 아이템 입력'
+    )
+
+    inputbox.send_keys('공작깃털 사기')
+
+    inputbox.send_keys(Keys.ENTER)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: 공작깃털 사기' for row in rows),
+      "신규 작업이 테이블에 표시되지 않는다"
+    )
+
+
+    self.fail('Finish the test')
+  
+
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
