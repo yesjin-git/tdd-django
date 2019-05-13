0e8b303e8ba65f1ffd5ef1c9212f1a8d2814b83b
tiuln2010 <destiy@naver.com>
2019-05-13 21:55:52 +0900

    feat: Chapter answer

--- lists/tests.py
+++ lists/tests.py
@@ -38,27 +38,27 @@ class HomePageTest(TestCase):
 
   def test_home_page_can_save_a_POST_request(self):
     response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
-    self.assertEqual(Item.objects.count(),1)
 
+    setl.assertEqual(Item.objects.count(), 1)
     new_item = Item.objects.first()
     self.assertEqual(new_item.text, '신규 작업 아이템')
 
-    self.assertIn(response.content.decode(), '신규 작업 아이템')
-
   def test_home_page_only_save_items_when_necessary(self):
-    request = HttpRequest()
-    home_page(request)
+    self.client.get('/')
     self.assertEqual(Item.objects.count(),0)
 
   def test_home_page_redirects_after_POST(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = '신규 작업 아이템'
-
-    response = home_page(request)
-
+    request = self.client.post('/', data={'item_text': 'A new list item'})
     self.assertEqual(response.status_code, 302)
     self.assertEqual(response['location'], '/')
 
+  def test_displays_all_list_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/')
+
+    self.assertIn('itemey 1', response.content.decode())
+    self.assertIn('itemey 2', response.content.decode())
 
 # Create your tests here.
